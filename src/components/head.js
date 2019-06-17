ed79fe9f6cc03e1e4e3c2d463a537f59a6f3814b
Nikos Pap <admin@nikospap.com>
2019-06-17 17:31:24 +0300

    add canonical

--- src/components/head.js
+++ src/components/head.js
@@ -51,6 +51,8 @@ const Head = ({ metadata }) => (
     <meta name="twitter:image" content={`${config.siteUrl}${ogImage}`} />
     <meta name="twitter:image:alt" content={metadata.title} />
 
+    <link rel="canonical" href={metadata.siteUrl} />
+
     <link rel="apple-touch-icon" sizes="57x57" href={appleIcon57x57} />
     <link rel="apple-touch-icon" sizes="60x60" href={appleIcon60x60} />
     <link rel="apple-touch-icon" sizes="72x72" href={appleIcon72x72} />
