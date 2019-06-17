b52e7ef2f7b0c541e89ff8dc3e70ba24b818b556
Nikos Pap <admin@nikospap.com>
2019-06-17 18:07:25 +0300

    add google verify code

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ module.exports = {
   siteUrl: 'https://nikospap.com',
   siteLanguage: 'en_US',
 
-  // googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  googleVerification: 'KSTWsfvBPhaV7mGXTCEN0_2BNNpeCzGgnOscpXiZ4dU',
 
   name: 'Nikos Papageorgiou',
   location: 'Athens, Greece',
